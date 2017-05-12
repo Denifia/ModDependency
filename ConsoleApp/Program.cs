1c4867e0717306741c247a38202ce1884d91ee16
Luke Wale <mr.l.wale@gmail.com>
2017-05-12 15:38:10 +0800

    prepared to start writing algorithm

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,7 +15,45 @@ namespace ConsoleApp
 
         public static IMod[] Sort(IMod[] mods)
         {
-            return mods;
+            // Kahn's algorithm (https://en.wikipedia.org/wiki/Topological_sorting)
+            //L ← Empty list that will contain the sorted elements
+            //S ← Set of all nodes with no incoming edges
+            //while S is non-empty do
+            //    remove a node n from S
+            //    add n to tail of L
+            //    for each node m with an edge e from n to m do
+            //        remove edge e from the graph
+            //        if m has no other incoming edges then
+            //            insert m into S
+            //if graph has edges then
+            //    return error (graph has at least one cycle)
+            //else 
+            //    return L (a topologically sorted order)
+
+            var unsortedMods = mods.ToList();
+            var sortedMods = new List<IMod>();
+            var modsWithIssues = new Dictionary<string, string>();
+
+            do
+            {
+                var mod = unsortedMods[0];
+                unsortedMods.RemoveAt(0);
+                //foreach (var modId in mod.ModManifest.LoadBefore)
+                //{
+                //    if (ModIdFoundInList(modId, sortedMods))
+                //    {
+
+                //    }
+                //}
+                sortedMods.Add(mod);
+            } while (unsortedMods.Any());
+                
+            return sortedMods.ToArray();
+        }
+
+        public static bool ModIdFoundInList(string modId, List<IMod> mods)
+        {
+            return mods.Any(x => x.ModManifest.UniqueID.Equals(modId));
         }
     }
 
@@ -64,8 +102,39 @@ namespace ConsoleApp
             get => _uniqueID;
             set => _uniqueID = value;
         }
-        public string[] LoadBefore { get; set; }
-        public string[] LoadAfter { get; set; }
+
+        private string[] _loadBefore = new string[0];
+        public string[] LoadBefore {
+            get => _loadBefore;
+            set
+            {
+                if (value == null)
+                {
+                    _loadBefore = new string[0];
+                }
+                else
+                {
+                    _loadBefore = value;
+                }
+            }
+        }
+
+        private string[] _loadAfter = new string[0];
+        public string[] LoadAfter
+        { 
+            get => _loadAfter;
+            set
+            {
+                if (value == null)
+                {
+                    _loadAfter = new string[0];
+                }
+                else
+                {
+                    _loadAfter = value;
+                }
+            }
+        }
 
         public Manifest(string id)
         {

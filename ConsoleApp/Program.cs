4aab6778a1e7cc82e621b422e23df497faa1d8d1
Luke Wale <mr.l.wale@gmail.com>
2017-05-12 14:52:25 +0800

    initial setup of project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static IMod[] Sort(IMod[] mods)
+        {
+            return mods;
+        }
+    }
+
+    public interface IMod
+    {
+        IManifest ModManifest { get; }
+        void Entry();
+    }
+
+    public class Mod : IMod
+    {
+        private IManifest _manifest;
+        public IManifest ModManifest => _manifest;
+
+        public Mod(string id)
+        {
+            _manifest = new Manifest(id);
+        }
+
+        public void Entry()
+        {
+            Console.WriteLine($"Called entry for {_manifest.UniqueID}");
+        }
+    }
+
+    public interface IManifest
+    {
+        string UniqueID { get; set; }
+    }
+
+    public class Manifest : IManifest
+    {
+        private string _uniqueID;
+        public string UniqueID {
+            get => _uniqueID;
+            set => _uniqueID = value;
+        }
+
+        public Manifest(string id)
+        {
+            _uniqueID = id;
+        }
+    }    
+}
---
+++ UnitTests/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var sortedMods = Program.Sort(new IMod[1] { new Mod("a") });
+            Assert.AreEqual(1, sortedMods.Length);
+        }
+    }
+}

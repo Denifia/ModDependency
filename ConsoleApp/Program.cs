e2fa31e556db3bf38dd93c7a8f6312b5970b595a
Luke Wale <mr.l.wale@gmail.com>
2017-05-13 11:21:39 +0800

    now checking for circular references

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,7 +11,18 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            var a = new Mod("a");
+            // Success
+            var a = new Mod("a", loadBefore: Mods("c"));
+            var b = new Mod("b", loadBefore: Mods("a"));
+            var c = new Mod("c", loadAfter: Mods("e"));
+            var d = new Mod("d", loadAfter: Mods("b"), loadBefore: Mods("e"));
+            var e = new Mod("e", loadAfter: Mods("a"));
+            var f = new Mod("f");
+            var g = new Mod("g");
+            var mods = new List<IMod>() { a, b, c, d, e, f, g };
+
+            // Circular
+            var a = new Mod("a", loadBefore: Mods("b"));
             var b = new Mod("b", loadBefore: Mods("a"));
             var mods = new List<IMod>() { a, b };
 
@@ -19,18 +30,21 @@ namespace ConsoleApp
             Console.WriteLine("-----");
             foreach (var mod in mods)
             {
-                Console.WriteLine($"\"{mod.ModManifest.UniqueID}\" \n  LoadsBefore: {string.Join(", ", mod.ModManifest.LoadBefore)} \n  LoadsAfter: {string.Join(", ", mod.ModManifest.LoadAfter)}");
+                Console.WriteLine($"\"{mod.ModManifest.UniqueID}\" ( LoadsBefore: {string.Join(", ", mod.ModManifest.LoadBefore)} | LoadsAfter: {string.Join(", ", mod.ModManifest.LoadAfter)} )");
             }
 
+            Console.WriteLine();
+            Console.WriteLine();
             var sortedMods = Sort(mods);
 
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("OUTPUT");
-            Console.WriteLine("------");
-            foreach (var mod in sortedMods)
+            if (sortedMods.Any())
             {
-                Console.WriteLine($"Loaded \"{mod.ModManifest.UniqueID}\" \n  LoadsBefore: {string.Join(", ", mod.ModManifest.LoadBefore)} \n  LoadsAfter: {string.Join(", ", mod.ModManifest.LoadAfter)}");
+                Console.WriteLine("OUTPUT");
+                Console.WriteLine("------");
+                foreach (var mod in sortedMods)
+                {
+                    Console.WriteLine($"Loaded \"{mod.ModManifest.UniqueID}\" ( LoadsBefore: {string.Join(", ", mod.ModManifest.LoadBefore)} | LoadsAfter: {string.Join(", ", mod.ModManifest.LoadAfter)} )");
+                }
             }
             Console.ReadLine();
         }
@@ -43,18 +57,27 @@ namespace ConsoleApp
             var sortedMods = new Stack<IMod>();
             var visitedMods = new bool[mods.Count];
 
+            var currentChain = new List<IMod>();
+            var success = true;
             for (int modIndex = 0; modIndex < mods.Count; modIndex++)
             {
                 if (visitedMods[modIndex] == false)
                 {
-                    TopologicalSort(modIndex, visitedMods, sortedMods);
+                    success = TopologicalSort(modIndex, visitedMods, sortedMods, currentChain);
                 }
+                if (!success) break;
             }
             
+            if (!success)
+            {
+                // Failed to sort list, return no mods.
+                return new List<IMod>();
+            }
+
             return sortedMods.Reverse().ToList();
         }
 
-        private static void TopologicalSort(int modIndex, bool[] visitedMods, Stack<IMod> sortedMods)
+        private static bool TopologicalSort(int modIndex, bool[] visitedMods, Stack<IMod> sortedMods, List<IMod> currentChain)
         {
             visitedMods[modIndex] = true;
             var mod = UnsortedMods[modIndex];
@@ -63,16 +86,36 @@ namespace ConsoleApp
                 x.ModManifest.LoadBefore.Contains(mod.ModManifest.UniqueID)
             ).ToList();
 
+            var circularReference = currentChain.FirstOrDefault(x => modsToLoadFirst.Contains(x));
+            if (circularReference != null)
+            {
+                Console.WriteLine($"Circular reference found.");
+                var chain = $"{mod.ModManifest.UniqueID} -> {circularReference.ModManifest.UniqueID}";
+                for (int i = currentChain.Count - 1; i >= 0; i--)
+                {
+                    chain = $"{currentChain[i].ModManifest.UniqueID} -> " + chain;
+                    if (currentChain[i].ModManifest.UniqueID.Equals(mod.ModManifest.UniqueID)) break;
+                }
+                Console.WriteLine(chain);
+                return false;
+            }
+
+            currentChain.Add(mod);
+
+            var success = true;
             foreach (var requiredMod in modsToLoadFirst)
             {
                 var index = UnsortedMods.IndexOf(requiredMod);
                 if (!visitedMods[index])
                 {
-                    TopologicalSort(index, visitedMods, sortedMods);
+                    success = TopologicalSort(index, visitedMods, sortedMods, currentChain);
                 }
+                if (!success) break;
             }
 
             sortedMods.Push(mod);
+            currentChain.Remove(mod);
+            return success;
         }
 
         public static string[] Mods(params string[] mods)
